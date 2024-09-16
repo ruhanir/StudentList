1b9ad364e28b7ed33a07a11da10ae0fffa36b045
dudej <dudej@10.56.30.43>
2024-09-16 10:38:17 -0400

    PartTimeStudent file added

---
+++ src/studentlist/partTimeStudent.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author dudej
+ */
+public class partTimeStudent extends Student{
+    private int numCourses;
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+    
+    
+}
