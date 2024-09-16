3233356bf8f4828c1ddb7aa8aac9301d0aa891fe
dudej <dudej@10.56.30.43>
2024-09-16 09:35:45 -0400

    Getter setter are added

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -21,5 +21,13 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String add) {
+        this.address = add;
+    }
    
 }
